44adf42f5c050f9e44a81125293b547f56bdc03a
RoBdUcK <thisisctfacc01@gmail.com>
2022-02-25 20:18:58 +0530

    FPD_new_formula

--- fpd_new_formula.py
+++ fpd_new_formula.py
@@ -1,21 +1,16 @@
 import math
-# mm
 
-L = 500
-print_repeat_length = L
-base_layer_thickness = 0.1
-t = 2.84
-
-def main():
+def main(L, base_layer_thickness, t):
+    print_repeat_length = L
     K = 2 * math.pi * (t-base_layer_thickness)
 
+    # Getting .xx (Decimal int)
     K = str(K)[0:5]
-    
 
     FPD = ((print_repeat_length-float(K))/print_repeat_length)*100
-
     FPD = str(FPD)[0:5]
-    print("This is K : ", K)
-    print(f"This is FPD : {FPD}%")
 
-main()
+    print(f"K = {K}")
+    print(f"FPD = {FPD}%")
+
+main(500, 0.1, 2.84)
